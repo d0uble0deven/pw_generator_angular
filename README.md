f59910fb5f8bf09208b92016fd07b90ef9567e8c
Dev Govindji <friendsofdev94@gmail.com>
2020-07-08 17:49:30 -0500

    completes app

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Pw
 
+[Deployed Link] (https://pw-zeta-hazel.vercel.app/)
+
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 10.0.1.
 
 ## Development server
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,62 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'pw';
+  length = 0;
+  includeSymbols = false;
+  includeNumbers = false;
+  includeLetters = false;
+  password = '';
+
+  onChangeLength(value: string) {
+    // all data from html is a string
+    const parsedValue = parseInt(value);
+
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
+
+  onButtonClick() {
+    // data to choose from
+    const numbers = '1234567890';
+    const letters = 'qwertyuiopasdfghjklzxcvbnm';
+    const symbols = '!@#$%^&*():"<>?,./-=|';
+    // characters that pw must be made of
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    // generateing actual pw by randomly selecting from validChars
+    let generatePassword = '';
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatePassword += validChars[index];
+    }
+    this.password = generatePassword;
+  }
+
+
+
+  onChangeUseLetters() {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers() {
+    this.includeNumbers = !this.includeNumbers;
+  }
+  onChangeUseSymbols() {
+    this.includeSymbols = !this.includeSymbols;
+  }
+
 }
+
